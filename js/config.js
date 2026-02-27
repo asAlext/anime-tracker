411b5f25b6f6f64af8b773b0210a77f1f9a3187f
asAlext <alex.t.33520@gmail.com>
2026-02-27 04:42:46 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,3 +3,4 @@ const SUPABASE_URL = 'https://adbahafsimtjcclvuqxa.supabase.co';  // ← remplac
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkYmFoYWZzaW10amNjbHZ1cXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NzM0MTQsImV4cCI6MjA4NzU0OTQxNH0.qyXqZAz-OYiZMPBISHtqJSeRvTVvpbXbK8JGaALKYZo'; // ← colle TA vraie clé anon ici
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+console.log("Supabase chargé depuis config.js :", supabase ? "OK" : "Erreur");
