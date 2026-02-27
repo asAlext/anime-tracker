209466d63c7bd78dcaea0cbd0f72ec08f7d5e876
asAlext <alex.t.33520@gmail.com>
2026-02-27 05:07:40 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,7 @@
+// SUPABASE CONFIG - UNIQUE DÉCLARATION
 const SUPABASE_URL = 'https://adbahafsimtjcclvuqxa.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkYmFoYWZzaW10amNjbHZ1cXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NzM0MTQsImV4cCI6MjA4NzU0OTQxNH0.qyXqZAz-OYiZMPBISHtqJSeRvTVvpbXbK8JGaALKYZo';
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 console.log("Supabase chargé depuis config.js :", supabase ? "OK" : "Erreur");
