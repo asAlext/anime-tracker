57b3e852bd1cb86d650dacddec95bef48565757d
asAlext <alex.t.33520@gmail.com>
2026-02-27 05:05:03 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,3 @@
-// === SUPABASE CONFIG ===
 const SUPABASE_URL = 'https://adbahafsimtjcclvuqxa.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkYmFoYWZzaW10amNjbHZ1cXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NzM0MTQsImV4cCI6MjA4NzU0OTQxNH0.qyXqZAz-OYiZMPBISHtqJSeRvTVvpbXbK8JGaALKYZo';
 
