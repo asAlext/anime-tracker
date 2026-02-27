c20fefc76549ae2a9132c54cb6a0db53a5004fc8
asAlext <alex.t.33520@gmail.com>
2026-02-27 04:53:11 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // === SUPABASE CONFIG ===
-const SUPABASE_URL = 'https://adbahafsimtjcclvuqxa.supabase.co';  // ← remplace si ton URL est différente
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkYmFoYWZzaW10amNjbHZ1cXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NzM0MTQsImV4cCI6MjA4NzU0OTQxNH0.qyXqZAz-OYiZMPBISHtqJSeRvTVvpbXbK8JGaALKYZo'; // ← colle TA vraie clé anon ici
+const SUPABASE_URL = 'https://adbahafsimtjcclvuqxa.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkYmFoYWZzaW10amNjbHZ1cXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NzM0MTQsImV4cCI6MjA4NzU0OTQxNH0.qyXqZAz-OYiZMPBISHtqJSeRvTVvpbXbK8JGaALKYZo';
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 console.log("Supabase chargé depuis config.js :", supabase ? "OK" : "Erreur");
